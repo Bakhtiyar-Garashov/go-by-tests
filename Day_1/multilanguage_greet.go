c8763bff7854e59cfc9963486d77232ccbb18710
Bakhtiyar-Garashov <bakhtiyar.garashov@gmail.com>
2023-01-31 22:36:08 +0200

    Refactor: using named returns

--- Day_1/multilanguage_greet.go
+++ Day_1/multilanguage_greet.go
@@ -2,8 +2,7 @@ package main
 
 import "fmt"
 
-func MultilanguageGreet(name, langCode string) string {
-	var message string
+func MultilanguageGreet(name, langCode string) (message string) {
 	switch langCode {
 	case "EE":
 		message = fmt.Sprintf("Tere tulemast, %s!", name)
@@ -11,6 +10,8 @@ func MultilanguageGreet(name, langCode string) string {
 		message = fmt.Sprintf("Salam, %s!", name)
 	case "EN":
 		message = fmt.Sprintf("Hello, %s!", name)
+	default:
+		message = fmt.Sprintf("Hello, %s!", name)
 	}
-	return message
+	return
 }
