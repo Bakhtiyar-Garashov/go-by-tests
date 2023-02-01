b61cec6aa800bd3b470cd02d0ae19174613c9b59
Bakhtiyar Garashov <bakhtiyar@Bakhtiyars-MBP.lan>
2023-02-01 14:39:10 +0200

    Day 2: fix utils package and use it

--- Day_1/multilanguage_greet_test.go
+++ Day_1/multilanguage_greet_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"testing"
 
-	utils "github.com/Bakhtiyar-Garashov/go-by-tests/utils"
+	"github.com/Bakhtiyar-Garashov/go-by-tests/utils"
 )
 
 func TestMultilanguageGreet(t *testing.T) {
@@ -16,12 +16,12 @@ func TestMultilanguageGreet(t *testing.T) {
 	t.Run("Greet Road Runner in Azerbaijani", func(t *testing.T) {
 		expected := "Salam, Road Runner!"
 		got := MultilanguageGreet("Road Runner", "AZ")
-		assertCorrectMessage(t, expected, got)
+		utils.AssertCorrectMessage(t, expected, got)
 	})
 
 	t.Run("Greet Road Runner in English", func(t *testing.T) {
 		expected := "Hello, Road Runner!"
 		got := MultilanguageGreet("Road Runner", "EN")
-		assertCorrectMessage(t, expected, got)
+		utils.AssertCorrectMessage(t, expected, got)
 	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bakhtiyar-garashov/go-by-tests
 
 go 1.19
+
+require github.com/Bakhtiyar-Garashov/go-by-tests/utils v0.0.0-20230201122045-380595d8152f // indirect
