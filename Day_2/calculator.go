514810c55fe8bd24c8bca14e9b62f339b2e68f80
Bakhtiyar Garashov <bakhtiyar@Bakhtiyars-MBP.lan>
2023-02-01 15:43:06 +0200

    Day 2: cover all cases with test

--- Day_2/calculator.go
+++ Day_2/calculator.go
@@ -1,5 +1,18 @@
 package main
 
 func Calculate(num1, num2 float64, operation string) (result float64) {
+	switch operation {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "/":
+		result = num1 / num2
+	case "*":
+		result = num1 * num2
+	default:
+		result = 0
+	}
+
 	return
 }
--- Day_2/calculator_test.go
+++ Day_2/calculator_test.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestCalculator(t *testing.T) {
+	t.Run("Addition", func(t *testing.T) {
+		expected := 4.5
+
+		result := Calculate(1.5, 3.0, "+")
+
+		if result != expected {
+			t.Errorf("expected '%f' but got '%f'", expected, result)
+		}
+
+	})
+
+	t.Run("Subtraction", func(t *testing.T) {
+		expected := 2.0
+
+		result := Calculate(3.0, 1.0, "-")
+
+		if result != expected {
+			t.Errorf("expected '%f' but got '%f'", expected, result)
+		}
+
+	})
+
+	t.Run("Division", func(t *testing.T) {
+		expected := 1.5
+
+		result := Calculate(3.0, 2.0, "/")
+
+		if result != expected {
+			t.Errorf("expected '%f' but got '%f'", expected, result)
+		}
+
+	})
+
+	t.Run("Multiplication", func(t *testing.T) {
+		expected := 4.5
+
+		result := Calculate(3.0, 1.5, "*")
+
+		if result != expected {
+			t.Errorf("expected '%f' but got '%f'", expected, result)
+		}
+
+	})
+
+	t.Run("Default case", func(t *testing.T) {
+		expected := 0.0
+
+		result := Calculate(3.0, 1.5, "%")
+
+		if result != expected {
+			t.Errorf("expected '%f' but got '%f'", expected, result)
+		}
+
+	})
+}
